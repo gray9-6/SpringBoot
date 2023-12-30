2af88dda3ef24d083ce21342a93e17c38fb3dae8
Ajay Kumar <aky22896@gmail.com>
2023-12-30 23:56:43 +0530

    learned about the controller layer of spring mvc

--- src/main/java/com/example/spring6webapp/controller/BookController.java
+++ src/main/java/com/example/spring6webapp/controller/BookController.java
@@ -17,7 +17,6 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model){
         model.addAttribute("books",bookService.findAll());
-
         return "books";
     }
 }
